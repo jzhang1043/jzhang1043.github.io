8c27f076967af85737d024665b1cd6ea236d261b
Zhang, Jiahao <jiahao4@illinois.edu>
2022-07-30 14:50:50 -0500

    update

--- app.js
+++ app.js
@@ -9,20 +9,57 @@ async function init() {
     var xs = d3.scaleLog().domain([10,150]).range([0, width]); 
     var ys = d3.scaleLog().domain([10,150]).range([height, 0]);
 
+
     svg.append("g").attr("transform", "translate("+margin+","+margin+")")
-        .selectAll()
+        .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
+            .attr("class", function (d) {return "dot " + d.Fuel;})
             .attr("cx",function(d) {return xs(d.AverageCityMPG);})
             .attr("cy",function(d) {return ys(d.AverageHighwayMPG);})
-            .attr("r", function(d) {return 2+d.EngineCylinders;})     
+            .attr("r", function(d) {return 2+d.EngineCylinders;})
+            .style("fill", function (d) {return color(d.Fuel);}) 
+            .on("mouseover", highlight)
+            .on("mouseleave", doNotHighlight)
+
+    // add y axis
     svg.append("g")
         .attr("transform", "translate("+margin+","+(margin)+")")
         .call(d3.axisLeft(ys).tickValues([10,20,50,100])
                 .tickFormat(d3.format("~s")))
-        svg.append("g")
-        .attr("transform", "translate("+(margin)+","+(height+margin)+")")
-        .call(d3.axisBottom(xs).tickValues([10,20,50,100])
-                .tickFormat(d3.format("~s")))
+    // add x axis
+    svg.append("g")
+    .attr("transform", "translate("+(margin)+","+(height+margin)+")")
+    .call(d3.axisBottom(xs).tickValues([10,20,50,100])
+            .tickFormat(d3.format("~s")))
+}
+
+var color = d3.scaleOrdinal()
+.domain(["setosa", "versicolor", "virginica" ])
+.range([ "#440154ff", "#21908dff", "#fde725ff"])
+
+var highlight = function(d){
+
+    Fuel_type = d.Fuel
+
+    d3.selectAll(".dot")
+      .transition()
+      .duration(200)
+      .style("fill", "lightgrey")
+      .attr("r", 3)
+
+    d3.selectAll("." + Fuel_type)
+      .transition()
+      .duration(200)
+      .style("fill", color(Fuel_type))
+      .attr("r", 7)
+}
+
+var doNotHighlight = function(){
+    d3.selectAll(".dot")
+    .transition()
+    .duration(200)
+    .style("fill", "lightgrey")
+    .attr("r", 5 )
 }
